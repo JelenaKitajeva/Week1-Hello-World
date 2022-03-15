f894e8ce21e46ad93d4c36a2d121d7aca65fc352
Jelena Kitajeva <lena_terehhova@mail.ru>
2022-03-15 11:22:16 +0200

    add my first project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hi_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is you firstname?");
+            string userFirstname = Console.ReadLine();
+            Console.WriteLine("What is you Lastname?");
+            string userLastname = Console.ReadLine();
+            Console.WriteLine($"Hello, {userFirstname} {userLastname}!");
+        }
+    }
+}
